d53861e86674a77ddff94ce7ca42e085a39e5748
Ryan McMillan <ryan@heliotrope.asg>
2014-10-27 15:32:39 +0000

    src/runge_mod.f90 modified to calculate Q_MNP approximately for any waveform. The a^3 factor is also taken into account in calculation of the P_MNP.

--- src/runge_mod.f90
+++ src/runge_mod.f90
@@ -10,10 +10,12 @@ CONTAINS
 SUBROUTINE runge
     USE double
     USE params , ONLY : num_lev, field, npts, rk_step, positions, mu, rho_0,  &
-                        out_file, npos
+                        out_file, npos, E0, omega_au, gma, trange_au, omega_ev,&
+                        rad, eps_eff1
     USE fields
-    USE global_params , ONLY : pi, ci
-    USE params , ONLY : theta, omega_g, gamma_g
+    USE global_params , ONLY : pi, ci, power_par, std_out
+    USE params , ONLY : omega_g, gamma_g, theta, en, s_alpha, eps_eff2
+    USE print_mat_mod
     IMPLICIT NONE
 
     ! rho(t)
@@ -36,6 +38,12 @@ SUBROUTINE runge
     COMPLEX(KIND=DP), ALLOCATABLE                 :: comm(:,:)
     ! Dipole at each time-step
     REAL(KIND=DP)                                 :: dipole
+    REAL(KIND=DP)  :: Q_mnp
+    REAL(KIND=DP)  :: abs_rate_0
+    REAL(KIND=DP)  :: abs_rate_approx
+    REAL(KIND=DP)  :: dpdt
+    REAL(KIND=DP)  :: Pe_old
+    REAL(KIND=DP)  :: E_mnp
     !
     ! Title format specifier
     CHARACTER(LEN=64)                             :: charfmat = '(ES22.14)'
@@ -75,9 +83,9 @@ SUBROUTINE runge
     fieldchar=' '//TRIM(field)//'(t)'
     WRITE(out_id, '(A22)', ADVANCE='NO') fieldchar
 !    WRITE(out_id, '(A22)', ADVANCE='NO') 'effective field '
-    WRITE(out_id, '(A22)', ADVANCE='NO') ' dipole (2-lev) '
-    WRITE(out_id, '(A22)', ADVANCE='NO') ' dipole (gold)  '
-!    WRITE(out_id, '(A16)', ADVANCE='NO') ' s(t) imag (~0?)'
+    WRITE(out_id, '(A22)', ADVANCE='NO') ' P_SQD          '
+    WRITE(out_id, '(A22)', ADVANCE='NO') ' P_MNP          '
+!    WRITE(out_id, '(A22)', ADVANCE='NO') ' d/dt P_MNP     '
     DO j = 1, npos 
         WRITE(out_id, '(A5)', ADVANCE='NO') ' rho_'
         WRITE(poschar, '(I2)') positions(j,1)
@@ -91,25 +99,31 @@ SUBROUTINE runge
     ! Printing values at t=0
     WRITE(out_id, charfmat, ADVANCE='NO') t
     WRITE(out_id, charfmat, ADVANCE='NO') efield(field, t)
-    WRITE(out_id, charfmat, ADVANCE='NO') efield(field, t)
     WRITE(out_id, charfmat, ADVANCE='NO') dipole
-    WRITE(out_id, charfmat, ADVANCE='NO') REAL(Pe)
-!    WRITE(out_id, charfmat, ADVANCE='NO') AIMAG(Pe)
+    WRITE(out_id, charfmat, ADVANCE='NO') Pe
+!    WRITE(out_id, charfmat, ADVANCE='NO') dpdt
     DO j = 1, npos
             WRITE(out_id, charfmat, ADVANCE='NO')                             &
             REAL(rho(positions(j,1),positions(j,2)),KIND=DP)
             WRITE(out_id, charfmat, ADVANCE='NO')                             &
-            IMAG(rho(positions(j,1),positions(j,2)))
+            AIMAG(rho(positions(j,1),positions(j,2)))
     ENDDO                                 
+
     WRITE(out_id,*)
 
-    ! Percentage complete
-    pcomp = REAL(npts)/pfraco
+!    ! Percentage complete
+!    pcomp = REAL(npts)/pfraco
 
     ! Parameters in RK algorithm
     fac(1) = 0.5_DP
     fac(2) = 0.5_DP
     fac(3) = 1.0_DP
+
+!    abs_rate = 0.0_DP
+    Q_mnp = 0.0_DP
+
+!theta = ci*2.0_DP*gamma_g*theta*(REAL(mu(1,2)*mu(1,2)))/REAL(gma(1,2))
+!theta = ci*2.0_DP*gamma_g*theta*3.40824295834338e+04
 
     DO i = 0, npts-1
 
@@ -132,105 +146,67 @@ SUBROUTINE runge
         rho = rho + (k_rho(1,:,:) + 2.0_DP*k_rho(2,:,:) + 2.0_DP*k_rho(3,:,:) + k_rho(4,:,:))/6.0_DP
         s = s + (k_s(1) + 2.0_DP*k_s(2) + 2.0_DP*k_s(3) + k_s(4))/6.0_DP
         t = t + rk_step
-        
-!        k_s = 0.0_DP
-!
-!        DO j = 1,4
-!
-!            IF ( j == 1 ) THEN
-!                s_tmp = s
-!                t_tmp = t
-!            ELSE
-!                s_tmp = s + fac(j-1)*k_s(j-1)
-!                t_tmp = t + fac(j-1)*rk_step
-!            ENDIF
-!
-!            k_s(j) = -(-ci*gamma_g + omega_g)*s_tmp &
-!                     -(efield(field, t_tmp) + 4.0*pi*dipole)
-!            k_s(j) = rk_step * k_s(j)
-!
-!        ENDDO
-!
-!        s = s + (k_s(1) + 2.0_DP*k_s(2) + 2.0_DP*k_s(3) + k_s(4))/6.0_DP
-!
-!        Pe = theta*s
-!
-!        ! Initialize RK variables
-!        k_rho = 0.0_DP
-!
-!        DO j = 1,4
-!            
-!            IF ( j == 1 ) THEN
-!                rho_tmp = rho
-!                t_tmp = t
-!            ELSE
-!                rho_tmp = rho + fac(j-1)*k_rho(j-1,:,:)
-!                t_tmp = t + fac(j-1)*rk_step
-!            ENDIF
-!
-!            field_t = efield(field, t_tmp) + 4.0_DP*pi*REAL(Pe)
-!            comm = commute(mu, rho_tmp)
-!
-!            DO n = 1,num_lev
-!                DO m = 1,num_lev
-!                    IF ( m >= n ) THEN
-!                        k_rho(j,n,m) = runge_k_nm(n, m, field_t, rho_tmp, comm)
-!                    ENDIF
-!                ENDDO
-!            ENDDO
-!
-!        ENDDO
-!        
-!        rho = rho + (k_rho(1,:,:) + 2.0_DP*k_rho(2,:,:) +                             &
-!                     2.0_DP*k_rho(3,:,:) + k_rho(4,:,:)) / 6.0_DP
-!
-!        ! Exploiting that p_nm=p_mn*
-!        DO n = 1,num_lev
-!            DO m =1,num_lev
-!                IF ( m > n ) THEN
-!                    rho(m,n) = CONJG(rho(n,m))
-!                ENDIF
-!            ENDDO
-!        ENDDO
 
         ! Calculating dipole
-        dipole = 0.0_DP
-        DO n = 1,num_lev
-            DO m = 1,num_lev
-                dipole = dipole + REAL(rho(n,m) * mu(m,n), KIND=DP)
-            ENDDO
-        ENDDO
+!        dipole = 0.0_DP
+!        DO n = 1,num_lev
+!            DO m = 1,num_lev
+!                dipole = dipole + REAL(rho(n,m) * mu(m,n))
+!            ENDDO
+!        ENDDO
 
+        dipole = trace(MATMUL(rho,mu))
+
+        Pe_old = Pe
         ! Calculating gold dipole
-        Pe = REAL(theta*s)
+        Pe = rad**3*theta*2.0_DP*REAL(s)
+!        Pe = theta*2.0_DP*REAL(s)
 
-!        ! Next time-step
-!        t   = t + rk_step
+        dpdt = (Pe - Pe_old)/rk_step
+        E_mnp = efield(field,t) + s_alpha*dipole/eps_eff2/rad**3
+
+        Q_mnp = Q_mnp + dpdt*E_mnp
+
+!        abs_rate = abs_rate + SIN(omega_au*t)*(Pe + dipole)
+!        abs_rate = abs_rate + SIN(omega_au*t)*(Pe)
 
         !! PRINTING VALUES !!
         WRITE(out_id, charfmat, ADVANCE='NO') t  
         WRITE(out_id, charfmat, ADVANCE='NO') efield(field, t)
-!        WRITE(out_id, charfmat, ADVANCE='NO') efield(field, t) + efield(field, t) + 4.0_DP*pi*REAL(Pe)
         WRITE(out_id, charfmat, ADVANCE='NO') dipole
         WRITE(out_id, charfmat, ADVANCE='NO') Pe
-!        WRITE(out_id, charfmat, ADVANCE='NO') AIMAG(Pe)
+!        WRITE(out_id, charfmat, ADVANCE='NO') dpdt
         DO j = 1, npos
                 WRITE(out_id, charfmat, ADVANCE='NO')                         &
                 REAL(rho(positions(j,1),positions(j,2)),KIND=DP)
                 WRITE(out_id, charfmat, ADVANCE='NO')                         &
-                IMAG(rho(positions(j,1),positions(j,2)))
+                AIMAG(rho(positions(j,1),positions(j,2)))
         ENDDO                                 
         WRITE(out_id,*)
        
         ! Percentage complete
-        pcomp = 100.0*REAL(i)/REAL(npts-1)
-        IF (ABS(pcomp-pfrac) <= 1.0E-2) THEN
-            WRITE(*,'(A1,A12,I3,A13)',ADVANCE='NO') char(13),                 &
-                  '||------->  ', NINT(pcomp), '%  <-------||'
-              pfrac = pfrac+pfraco
-        ENDIF
+!        pcomp = 100.0*REAL(i)/REAL(npts-1)
+!        IF (ABS(pcomp-pfrac) <= 1.0E-2) THEN
+!            WRITE(*,'(A1,A12,I3,A13)',ADVANCE='NO') char(13),                 &
+!                  '||------->  ', NINT(pcomp), '%  <-------||'
+!              pfrac = pfrac+pfraco
+!        ENDIF
 
     ENDDO
+
+!    abs_rate = 4.0_DP/3.0_DP*pi*rad**3*E0/trange_au*(Pe*COS(omega_au*trange_au) + omega_au*rk_step*abs_rate)
+!    abs_rate_0 = 1.0_DP/3.0_DP*E0**2 * rad**6 *omega_au * theta *gamma_g/((omega_g-omega_au)**2 + gamma_g**2)
+
+    Q_mnp = 4.0_DP/3.0_DP*pi*rad**3*Q_mnp*rk_step/trange_au
+    Q_mnp = Q_mnp*0.5_DP/pi
+
+    WRITE(std_out,charfmat, ADVANCE='NO') omega_ev
+    WRITE(std_out,charfmat) Q_mnp
+!    WRITE(0,charfmat, ADVANCE='NO') abs_rate*0.5_DP/pi
+!    WRITE(0,charfmat, ADVANCE='NO') abs_rate_0
+!    WRITE(0,charfmat, ADVANCE='NO') abs_rate_approx*0.5_DP/pi
+!    WRITE(0,charfmat) 4.0_DP/3.0_DP*pi*151.0_DP*REAL(omega_au*mu(1,2)**2*E0**2*gma(1,2)/(2.0_DP*eps_eff1*((en(2)-omega_au)**2+gma(1,2)**2)))
+!    WRITE(0,charfmat) REAL(omega_au*mu(1,2)**2*E0**2*gma(1,2)/(2.0_DP*eps_eff1*((en(2)-omega_au)**2+gma(1,2)**2)))
 
     CLOSE(out_id)
 
@@ -239,7 +215,7 @@ END SUBROUTINE runge
 SUBROUTINE rk_de(rho_in, s_in, t_in, rho_out, s_out)
     USE double
     USE params , ONLY : s_alpha, eps_eff1, eps_eff2, dist, theta, omega_g, &
-                            gamma_g
+                            gamma_g, rad
     USE params , ONLY : en, gma, field, mu, num_lev, rho_eq
     USE global_params , ONLY : ci
     USE fields , ONLY : efield
@@ -251,20 +227,25 @@ SUBROUTINE rk_de(rho_in, s_in, t_in, rho_out, s_out)
     COMPLEX(KIND=DP), INTENT(OUT) :: rho_out(num_lev,num_lev)
     COMPLEX(KIND=DP) :: comm(num_lev,num_lev)
     COMPLEX(KIND=DP), INTENT(OUT) :: s_out
+    REAL(KIND=DP) :: P_mnp, E_sqd, E_mnp
 
     INTEGER :: n, m
+
+    P_mnp = rad**3*theta*2.0_DP*REAL(s_in)
+!    P_mnp = theta*2.0_DP*REAL(s_in)
+    E_sqd = efield(field,t_in) + s_alpha*P_mnp/(eps_eff1*dist**3)
+    E_mnp = efield(field,t_in) + s_alpha*trace(MATMUL(mu,rho_in))/(eps_eff2*dist**3)
 
     comm = commute(mu, rho_in)
     DO n=1,num_lev
         DO m=1,num_lev
-           rho_out(n,m) = -ci*(en(n)-en(m))*rho_in(n,m) - gma(n,m)*(rho_in(n,m)&
-                          - rho_eq(n,m)) + ci*(efield(field,t_in)              &
-                          + s_alpha*theta*s_in/(eps_eff1*dist**3))          &
-                            *comm(n,m)
+           rho_out(n,m) = -ci*(en(n)-en(m))*rho_in(n,m)                 &
+                          - gma(n,m)*(rho_in(n,m) - rho_eq(n,m))        &
+                          + ci*E_sqd*comm(n,m)
         ENDDO
     ENDDO
 
-    s_out = -(gamma_g + ci*omega_g)*s_in + (efield(field,t_in) + s_alpha*trace(MATMUL(mu,rho_in))/(eps_eff2*dist**3))
+    s_out = -(gamma_g + ci*omega_g)*s_in + ci*E_mnp
 
 END SUBROUTINE rk_de
 

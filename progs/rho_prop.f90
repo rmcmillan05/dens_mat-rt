1f01a4b38e53921711f0393278cf7ce2c491ebf6
Ryan McMillan <rmcmillan05@qub.ac.uk>
2015-01-20 13:52:59 +0000

    Changed how field parameters are varied in rho_prop.exe

--- progs/rho_prop.f90
+++ progs/rho_prop.f90
@@ -11,14 +11,15 @@ PROGRAM rho_prop
     USE communications
     USE runge_mod , ONLY : runge
     USE params , ONLY : npts_per_proc, nprocs, omega,  &
-                        omega_from, remainder, proc_id, &
+                        field_param_from, remainder, proc_id, &
                         get_params, out_file, params_file, out_folder, jname, in_file, &
-                        omega_step, field, print_field_params
+                        field_param_step, field, print_field_params, field_width
     USE print_mod
     USE params , ONLY : check_file
     IMPLICIT NONE
 
     CHARACTER(LEN=8) :: suffix
+    INTEGER :: suff_int
     INTEGER :: j
 
     CALL set_up_mpi
@@ -34,17 +35,15 @@ PROGRAM rho_prop
 
     CALL get_params
 
-    SELECTCASE ( field )
+        DO j = 1, npts_per_proc + 1
 
-    CASE ( 'cosfield' )
-        DO j = 1, npts_per_proc
-            IF ( j == npts_per_proc .AND. proc_id < remainder ) THEN
-                omega = REAL(npts_per_proc*nprocs + proc_id)*omega_step + omega_from
-                WRITE(suffix,'(I8.8)') npts_per_proc*nprocs + proc_id + 1
-            ELSE
-                omega = REAL(proc_id*npts_per_proc + j - 1)*omega_step + omega_from
-                WRITE(suffix,'(I8.8)') proc_id*npts_per_proc + j
+            IF ( j == npts_per_proc + 1 .AND. proc_id >= remainder ) THEN
+                EXIT
             ENDIF
+
+            CALL change_parameter(j, suff_int)
+
+            WRITE(suffix,'(I8.8)') suff_int + 1
 
             out_file    = TRIM(out_folder)//'/'//TRIM(jname)//'_'//suffix//'.out'
             params_file = TRIM(out_folder)//'/'//TRIM(jname)//'_'//suffix//'.field_params'
@@ -53,15 +52,48 @@ PROGRAM rho_prop
             CALL print_field_params
         ENDDO
 
-    CASE DEFAULT
-        IF ( proc_id == 0 ) THEN
-            out_file = TRIM(out_folder)//'/'//TRIM(jname)//'.out'
-            params_file = TRIM(out_folder)//'/'//TRIM(jname)//'.params'
+!    CASE DEFAULT
+!        IF ( proc_id == 0 ) THEN
+!            out_file = TRIM(out_folder)//'/'//TRIM(jname)//'.out'
+!            params_file = TRIM(out_folder)//'/'//TRIM(jname)//'.params'
+!
+!            CALL runge
+!            CALL print_field_params
+!        ENDIF
+!
+!    END SELECT
 
-            CALL runge
-            CALL print_field_params
-        ENDIF
+CONTAINS
 
+SUBROUTINE change_parameter(i, suffix_int)
+    USE double
+    USE mpi
+    USE communications
+    USE params , ONLY : field_change_param, field_height, field_width, &
+                        field_param_step, field_param_from, omega
+    IMPLICIT NONE
+
+    INTEGER, INTENT(IN) :: i
+    INTEGER, INTENT(OUT) :: suffix_int
+    REAL(KIND=DP) :: out_val
+
+    IF ( i == npts_per_proc + 1 ) THEN
+        suffix_int = npts_per_proc*nprocs + proc_id
+        out_val = REAL(suffix_int)*field_param_step + field_param_from 
+    ELSE
+        suffix_int = proc_id*npts_per_proc + i - 1
+        out_val = REAL(suffix_int)*field_param_step + field_param_from
+    ENDIF
+
+    SELECTCASE ( field_change_param )
+        CASE ( 'field_height' )
+            field_height  = out_val
+        CASE ( 'field_width' )
+            field_width  = out_val
+        CASE ( 'omega' )
+            omega        = out_val
     END SELECT
+
+END SUBROUTINE change_parameter
 
 END PROGRAM rho_prop
